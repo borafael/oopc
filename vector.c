fd516e4178d065a4d865bd291eb527c5f4267792
Rafael Barrera Oro <borafael@gmail.com>
2018-04-28 23:04:51 -0300

    added methods

--- vector.c
+++ vector.c
@@ -19,6 +19,14 @@ float norm(Vector* v) {
 	return sqrt(v->x * v->x + v->y * v->y);
 }
 
+Vector* change_x(Vector* v, float x) {
+	return create_vector(x, v->y);
+}
+
+Vector* change_y(Vector* v, float y) {
+	return create_vector(v->x, y);
+}
+
 Vector* scale(Vector* v, float s) {
 	float n = norm(v);
 
@@ -27,6 +35,18 @@ Vector* scale(Vector* v, float s) {
 	}
 
 	return create_vector(s * (v->x / n), s * (v->y / n));
+}
+
+Vector* add(Vector* v1, Vector* v2) {
+	return create_vector(v1->x + v2->x, v1->y + v2->y);
+}
+
+Vector* sub(Vector* v1, Vector* v2) {
+	return create_vector(v1->x - v2->x, v1->y - v2->y);
+}
+
+Vector* mul(Vector* v, float k) {
+	return create_vector(v->x * k, v->y * k);
 }
 
 void destroy_vector(Vector* v) {
--- vector.h
+++ vector.h
@@ -17,7 +17,17 @@ float get_y(Vector* v);
 
 float norm(Vector* v);
 
+Vector* change_x(Vector* v, float x);
+
+Vector* change_y(Vector* v, float y);
+
 Vector* scale(Vector* v, float s);
+
+Vector* add(Vector* v1, Vector* v2);
+
+Vector* sub(Vector* v1, Vector* v2);
+
+Vector* mul(Vector* v, float k);
 
 void destroy_vector(Vector* vector);
 
